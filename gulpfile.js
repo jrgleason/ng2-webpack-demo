a7ea07e7f313e0119803f84a7aec992287cdf0f2
Jackie Gleason <jackiegleason@gmail.com>
2016-06-28 20:36:27 -0400

    Making it actually use grunt.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,12 @@
-var gulp = require('gulp');
-var webpack = require('webpack-stream');
-var named = require('vinyl-named');
+var gulp = require('gulp'),
+    webpack = require('webpack-stream'),
+    debug = require('gulp-debug'),
+    named = require('vinyl-named');
 gulp.task('default', function() {
     return gulp.src(['src/polyfills.js', 'src/vendor.ts', 'src/main.ts'])
+        .pipe(debug())
         .pipe(named(function(file){
-            if(file.basename == "main.ts"){
-                return 'app.js'
-            }
-            else{
-                file.extname = 'js';
-            }
         }))
+        .pipe(debug())
         .pipe(webpack())
 });
