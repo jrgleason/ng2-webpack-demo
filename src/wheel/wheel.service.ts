1e637fd793a9cb71f69abc0b7ad22c94202ac3c9
jrgleason <jackiegleason@gmail.com>
2016-07-15 13:48:47 -0400

    3 Main things
    1.) Upgrading Angular
    2.) Fixing scroll on OSX
    3.) Making the HTML actual inject properly

--- src/wheel/wheel.service.ts
+++ src/wheel/wheel.service.ts
@@ -3,10 +3,21 @@ import { Injectable, EventEmitter } from '@angular/core';
 @Injectable()
 export class WheelService {
     wheelEmitter = new EventEmitter();
+    private prevTime = new Date().getTime();
     constructor(){
         window.addEventListener("wheel", (e: any) => {
-            let direction = e.deltaY < 0 ? -1 : 1;
-            this.wheelEmitter.emit({direction: direction})
+          var curTime = new Date().getTime();
+          if(this.prevTime){
+            var timeDiff = curTime-this.prevTime;
+            if(timeDiff>500 && e.deltaY !== 0){
+              let direction = e.deltaY < 0 ? -1 : 1;
+              this.wheelEmitter.emit({direction: direction});
+              this.prevTime = curTime;
+            }
+          }
+          else{
+            this.prevTime = curTime;
+          }
         });
     }
 }
